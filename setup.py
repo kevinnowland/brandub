f1b9a1a5cc5135d588c3d4666149b4bde1df8ec6
knowland <knowland@covermymeds.com>
2020-12-06 17:48:13 -0500

    update setup

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name="brandub",
-    version="0.1",
+    version="1.0",
     description="play brandub",
     long_description=long_description,
     long_description_content_type='text/markdown',
