156db3f1d393d865901ace8f9824ce36205e0e35
knowland <knowland@covermymeds.com>
2020-12-06 15:38:18 -0500

    add setup.py,  move init file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="brandub",
+    version="0.1",
+    description="play brandub",
+    url="github.com/kevinnowland/brandub",
+    author="Kevin Nowland",
+    license="MIT",
+    packages=["brandub"],
+    zip_safe=False
+)
